c0668f94699dc5c535d56aef2040b53a505f7d78
binw4dev <binw4dev@gmail.com>
2025-10-13 16:42:09 -0400

    migrate with mongodb atlas

--- backend/app/config.py
+++ backend/app/config.py
@@ -7,5 +7,5 @@ GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
 JWT_SECRET = os.getenv("JWT_SECRET", "secret")
 JWT_ALGORITHM = os.getenv("JWT_ALGORITHM", "HS256")
 
-MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://binw4dev_db_user:Wcwe1IgdkVh5CsHr@bavilion-cluster-dev.cuylfau.mongodb.net/?retryWrites=true&w=majority&appName=bavilion-cluster-dev")
+MONGO_URI = os.getenv("MONGO_URI", "")
 MONGO_DB = os.getenv("MONGO_DB", "google-auth-demo")
