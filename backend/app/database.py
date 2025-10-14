3117fd5c7d350dcd80d49b9c41aebc636cc52c9f
binw4dev <binw4dev@gmail.com>
2025-10-13 21:29:52 -0400

    enable tls

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,7 +2,7 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from .config import MONGO_URI, MONGO_DB
 
 # create global async MongoDB client
-client = AsyncIOMotorClient(MONGO_URI)
+client = AsyncIOMotorClient(MONGO_URI, tls=True, tlsAllowInvalidCertificates=True)
 
 # database
 db = client[MONGO_DB]
